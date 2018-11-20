bba81790fcb6fbbe6a70cbc49bc74bc1e7f52727
Lorenzo Campanella <enzocampanella98@gmail.com>
2018-11-20 18:04:27 -0500

    use rel path in header

--- d3/workspace/EasyPay/src/pages/EasyPayBaseServlet.java
+++ d3/workspace/EasyPay/src/pages/EasyPayBaseServlet.java
@@ -18,9 +18,7 @@ public abstract class EasyPayBaseServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 9007811510279142478L;
-	
-	protected static String BASE_PATH = "/~lrc22/webapps8/";
-	
+		
 	protected EasyPayService _easyPayService;
 	
 	protected String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
