110b7b03f6bc7029e56e7414c050c74ec69bcb00
Lorenzo Campanella <enzocampanella98@gmail.com>
2018-12-11 23:11:48 -0500

    remove email addreses from phone table

--- d3/scripts/populate/populate_all.sql
+++ d3/scripts/populate/populate_all.sql
@@ -115,24 +115,7 @@ INSERT INTO `EmailAddress` (`Identifier`, `Verified`, `USSN`) VALUES
 INSERT INTO `Phone` (`Identifier`, `Verified`, `USSN`) VALUES
 ('111-222-3333', b'1', 183),
 ('4056057080', b'1', 1345728945),
-('732-123-4567', b'1', 7892),
-('amaclead@gmail.com', b'1', 660),
-('calbares@gmail.com', b'1', 624),
-('donette.foller@cox.net', b'1', 489),
-('gladys.rim@rim.org', b'1', 458),
-('gruta@cox.net', b'1', 559),
-('josephine_darakjy@darakjy.org', b'1', 514),
-('kiley.caldarera@aol.com', b'1', 751),
-('kris@gmail.com', b'1', 421),
-('leota@hotmail.com', b'1', 690),
-('lpaprocki@hotmail.com', b'1', 524),
-('mattie@aol.com', b'1', 449),
-('meaghan@hotmail.com', b'1', 477),
-('minna_amigon@yahoo.com', b'1', 461),
-('mitsue_tollner@yahoo.com', b'1', 505),
-('sage_wieser@cox.net', b'1', 646),
-('simona@morasca.com', b'1', 504),
-('yuki_whobrey@aol.com', b'1', 443);
+('732-123-4567', b'1', 7892);
 
 
 
